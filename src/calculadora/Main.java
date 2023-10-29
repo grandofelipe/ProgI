2e4ab1d763b7058b02aec431d7a7207f8fedd3b2
grandofelipe <felipegrando@uffs.edu.br>
2023-10-29 15:55:43 -0300

    Início da Interface Gráfica

--- src/calculadora/Main.java
+++ src/calculadora/Main.java
@@ -2,13 +2,41 @@ package calculadora;
 
 import calculadora.operacoes.Operacao;
 import calculadora.operacoes.Soma;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) {
-        Operacao soma = new Soma();
+        launch(args);
+        /*Operacao soma = new Soma();
         double x = 3.5, y = 7.43598374;
         System.out.println(x + soma.toString() + y + " = " + soma.executar(x, y));
         System.out.println(x + soma.toString() + y + " = " + soma.executar(x, y, 5));
         System.out.println(x + soma.toString() + y + " = " + soma.executar(x, y, 5));
+        */
+    }
+
+    @Override
+    public void start(Stage janelaPrincipal) throws Exception {
+        janelaPrincipal.setTitle("Título da Janela");
+        Button botao = new Button("Me clique");
+        botao.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent arg0) {
+                System.out.println("Clicou no botão");
+            }
+            
+        });
+
+        StackPane raiz = new StackPane();
+        raiz.getChildren().addAll(botao);
+        janelaPrincipal.setScene(new Scene(raiz, 400, 400));
+        janelaPrincipal.show();
     }
 }
